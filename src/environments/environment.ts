8587f0abfaa94bd3cdbaa3e956d52ba837942e9e
SantiagoOspinaO <santiago.m200@outlook.es>
2022-07-18 14:47:12 -0500

    Se crea el front del proyecto de Heroes

---
+++ src/environments/environment.ts
@@ -0,0 +1,29 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDfgZ3dqRpvlR0Mc4FqZ4kVihsLP5dMPKU',
+    authDomain: 'practica-firebase-3e6bc.firebaseapp.com',
+    projectId: 'practica-firebase-3e6bc',
+    storageBucket: 'practica-firebase-3e6bc.appspot.com',
+    messagingSenderId: '713590496254',
+    appId: '1:713590496254:web:464340a2b200d8e5bf3eaf',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
